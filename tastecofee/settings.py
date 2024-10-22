3f041c6a765b8594176f7b502a8c656f4c68e5b4
comiser <comiser@comiser.comiser>
2024-10-23 00:34:00 +0330

    update whitenoice

--- tastecofee/settings.py
+++ tastecofee/settings.py
@@ -241,7 +241,15 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
