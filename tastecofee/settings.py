cc70e41665cc3e16a43d97f1617643c44cdbad07
comiser <comiser@comiser.comiser>
2024-10-22 21:02:58 +0330

    add storage

--- tastecofee/settings.py
+++ tastecofee/settings.py
@@ -241,6 +241,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
