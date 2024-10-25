a034aea3a8c1377eb79ac2a700aa6cb09f4a9de8
comiser <comiser@comiser.comiser>
2024-10-25 16:47:23 +0330

    back to docker fi

--- tastecofee/settings.py
+++ tastecofee/settings.py
@@ -144,12 +144,9 @@ CKEDITOR_IMAGE_BACKEND = "pillow"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-
-
-        'NAME': 'persistent_db/db.sqlite3',    
-        }
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 
 
