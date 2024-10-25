4b3be4531a279f62aa1605955ad9db03042373c8
comiser <comiser@comiser.comiser>
2024-10-25 16:43:56 +0330

    back to docker fil

--- tastecofee/settings.py
+++ tastecofee/settings.py
@@ -130,25 +130,25 @@ CKEDITOR_IMAGE_BACKEND = "pillow"
 
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-    }
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-
-
-#         'NAME': 'persistent_db/db.sqlite3',    
-#         }
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": os.getenv("DB_NAME"),
+#         "USER": os.getenv("DB_USER"),
+#         "PASSWORD": os.getenv("DB_PASSWORD"),
+#         "HOST": os.getenv("DB_HOST"),
+#         "PORT": os.getenv("DB_PORT"),
+#     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+
+
+        'NAME': 'persistent_db/db.sqlite3',    
+        }
+}
 
 
 
